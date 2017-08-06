949024c9d3101968b9027f8e76c964245272ad8b
Hasan Tareque <hmtareque@gmail.com>
2017-08-06 15:57:50 +0100

    added the controller article and default laravel front asset enabled

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,27 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'ArticleController@index');
+
+Route::get('articles/not-actioned', 'ArticleController@notActioned');
+Route::get('articles/actioned', 'ArticleController@actioned');
+
+
+Route::get('articles/collect', 'ArticleController@collect');
+Route::post('articles/known/collect', 'ArticleController@storeKnownArticle');
+Route::post('articles/collect', 'ArticleController@store');
+
+
+Route::any('articles/confirm', 'ArticleController@confirm');
+
+Route::put('articles/mark-actioned', 'ArticleController@markActioned');
+Route::put('articles/unmark-actioned', 'ArticleController@unmarkActioned');
+Route::delete('articles/delete', 'ArticleController@destroy');
+
+
+Route::get('collections/history', 'ArticleController@collectionHistory');
+Route::get('collections/{collection_id}/articles', 'ArticleController@articleCollection');
+
+
+
+
