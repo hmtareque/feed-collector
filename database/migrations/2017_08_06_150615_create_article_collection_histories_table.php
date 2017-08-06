dca971d61b9a5acf4aa3ca92654aa474e0955c01
Hasan Tareque <hmtareque@gmail.com>
2017-08-06 16:12:01 +0100

    migration added and unused migration removed

---
+++ database/migrations/2017_08_06_150615_create_article_collection_histories_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticleCollectionHistoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('article_collection_histories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('source');
+            $table->string('rss_url');
+            $table->date('collected_at');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('article_collection_history');
+    }
+}
--- database/migrations/2017_08_06_150636_create_articles_table.php
+++ database/migrations/2017_08_06_150636_create_articles_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateArticlesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('articles', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('collection_id');
+            $table->string('headline');
+            $table->text('url');
+            $table->date('published_at');
+            $table->boolean('actioned')->default(0);
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
@@ -30,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('articles');
     }
 }
